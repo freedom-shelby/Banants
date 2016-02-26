040a400ef0392d64d0e2687b66b36c639f0f2415
ArmSALArmy <arsenchikh@gmail.com>
2016-02-26 10:56:57 +0400

    Pages Widgets Integretion

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -9,6 +9,6 @@ return array(
     '65fec9ebcfbb3cbb4fd0d519687aea01' => $vendorDir . '/danielstjules/stringy/src/Create.php',
     '72579e7bd17821bb1321b87411366eae' => $vendorDir . '/illuminate/support/helpers.php',
     '21e5fe9e3447fd509d76724235f80917' => $baseDir . '/config/database.php',
-    '8e1126fdb56b2e34cd39d610dc92c6f8' => $baseDir . '/routes/front.php',
     'af45600eb379ca2231d37ebe5aef1097' => $baseDir . '/routes/back.php',
+    '8e1126fdb56b2e34cd39d610dc92c6f8' => $baseDir . '/routes/front.php',
 );

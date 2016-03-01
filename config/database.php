2064a00e07e8f146deb2a69d1b5ecb1014b993f9
Hovhannisyan <arsen.hovhannisyan.1991@gmail.com>
2016-03-02 02:00:55 +0400

    Entitis-i modelneri kap@ dzel em

--- config/database.php
+++ config/database.php
@@ -19,10 +19,10 @@ $capsule = new Capsule();
 //]);
 $capsule->addConnection([
     'driver'    => 'mysql',
-    'host'      => 'www.horizondvp.org',
-    'database'  => 'horizon0_fcbanants',
-    'username'  => 'horizor0_banants',
-    'password'  => 'sKpER=e!Ph7J',
+    'host'      => 'localhost',
+    'database'  => 'banants',
+    'username'  => 'root',
+    'password'  => '',
     'charset'   => 'utf8',
     'collation' => 'utf8_general_ci',
     'prefix'    => '',

ddd812a83fd95137b30cd50e91143c988ff931c5
Hovhannisyan <arsen.hovhannisyan.1991@gmail.com>
2016-05-03 19:33:56 +0400

    Интеграция Меню в Админку

--- views/test/Test_1.php
+++ views/test/Test_1.php
@@ -1,37 +1,15 @@
 <div class="inner_content_wrapper">
-	<div class="inner_content stadion">
-		<div class="stadion_page">
-			<div class="stadion_info clearfix">
-				<div class="stadion_info_wrapper">
-					<h3><a href="#"><strong>Форма Клуба</strong></a></h3>
-					&laquo;Բանանցն&raquo; ավանդաբար հանդես է եկել մուգ կապույտ, սպիտակ մարզահագուսով, որը համալրված է նաև դեղին երանգներով: Ակումբի մարզաշապիկները, բնականաբար, տարեցտարի փոխվել են: Ներկայումս &laquo;Բանանցի&raquo; տնային մարզահագուստը մուգ կապույտ է՝ սպիտակ երանգներով, իսկ արտագնա խաղերի համար նախատեսված մարզաշապիկը սպիտակ է՝ կապույտ երանգներով:</div>				<div class="tshirt_photos_wrapper clearfix">
-					<div class="tshirt_wrapper clearfix">
-						<div class="tshirt">
-							<div class="">
-								<a class="fancybox" title="slideshow_images" href="/uploads/images/form/adidas3.jpg" rel="gallary_s">
-									<img style="width: 190px;" class="image" src="/uploads/images/form/adidas3.jpg" alt="Banants Stadium" />
-								</a>
-							</div>
-							<div class="">
-								<a class="fancybox" title="slideshow_images" href="/uploads/images/form/keeper2.jpg" rel="gallary_s">
-									<img style="width: 190px;" class="image" src="/uploads/images/form/keeper2.jpg" alt="Banants Stadium" />
-								</a>
-							</div>
-						</div>
-						<div class="tshirt">
-							<div class="">
-								<a class="fancybox" title="slideshow_images" href="/uploads/images/form/wihte.jpg" rel="gallary_s">
-									<img style="width: 190px;" class="image" src="/uploads/images/form/wihte.jpg" alt="Banants Stadium" />
-								</a>
-							</div>
-							<div class="">
-								<a class="fancybox" title="slideshow_images" href="/uploads/images/form/keeper1.png" rel="gallary_s">
-									<img style="width: 190px;" class="image" src="/uploads/images/form/keeper1.png" alt="Banants Stadium" />
-								</a>
-							</div>
-						</div>
-					</div>
+	<div class="inner_content article">
+		<div style="min-height: 173px;">
+			<div>
+				<h1>Сурен Балиян обслужит финал Кубка Армении</h1>
+				<div class="photo_wrapper" style="width: 265px;">
+					<a class="fancybox" title="Спорт Кафе" href="/uploads/images/news_6/yXyCgJiLHyAEqda4txl0tncKUL.jpg" rel="gallary_p">
+						<img class="image" style="max-width: 264px;" src="/uploads/images/news_6/yXyCgJiLHyAEqda4txl0tncKUL.jpg" alt="banants" />
+					</a>
 				</div>
+				<p>4-го мая, в 19:00, на стадионе "Республиканский" имени Вазгена Саргсяна состоится финальный матч Кубка Армении между командами "Бананц" и "Мика".</p>
+				<p>Арбитром матча назначен Сурен Балиян, судьи на линии - Месроп Казарян и Ваник Симонян. 4-м арбитром назначен Эрик Аревшатян, дополнительными помощниками будут Арман Алаберкян и Эрик Галстян.</p>
 			</div>
 		</div>
 	</div>

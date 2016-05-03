99d883d949dcd4ab9be701d304a89515de9b3d98
Hovhannisyan <arsen.hovhannisyan.1991@gmail.com>
2016-05-03 18:05:50 +0400

    Интеграция Меню в Админку

--- views/test/Test_1.php
+++ views/test/Test_1.php
@@ -4,20 +4,31 @@
 			<div class="stadion_info clearfix">
 				<div class="stadion_info_wrapper">
 					<h3><a href="#"><strong>Форма Клуба</strong></a></h3>
-					&ldquo;Бананц&rdquo; традиционно играет в темно-синей и белой спортивной форме, с желтыми окрасками. Естественно, из года в год изменилось дизайн игровой формы. Сейчас домашняя форма &ldquo;Бананца&rdquo; темно синая, с белыими окрасками, а выездная форма команды белая, с синими красками.</div>
-				<div class="tshirt_photos_wrapper clearfix">
+					&laquo;Բանանցն&raquo; ավանդաբար հանդես է եկել մուգ կապույտ, սպիտակ մարզահագուսով, որը համալրված է նաև դեղին երանգներով: Ակումբի մարզաշապիկները, բնականաբար, տարեցտարի փոխվել են: Ներկայումս &laquo;Բանանցի&raquo; տնային մարզահագուստը մուգ կապույտ է՝ սպիտակ երանգներով, իսկ արտագնա խաղերի համար նախատեսված մարզաշապիկը սպիտակ է՝ կապույտ երանգներով:</div>				<div class="tshirt_photos_wrapper clearfix">
 					<div class="tshirt_wrapper clearfix">
 						<div class="tshirt">
-							<div class="stadion">
-								<a class="fancybox" title="slideshow_images" href="/uploads/images/stadiums/001.jpg" rel="gallary_s">
-									<img class="image" src="/uploads/images/stadiums/001.jpg" alt="Banants Stadium" />
+							<div class="">
+								<a class="fancybox" title="slideshow_images" href="/uploads/images/form/adidas3.jpg" rel="gallary_s">
+									<img style="width: 190px;" class="image" src="/uploads/images/form/adidas3.jpg" alt="Banants Stadium" />
 								</a>
 							</div>
-
-							<img src="../../../uploads/images/form/11.jpg" alt="" />
+							<div class="">
+								<a class="fancybox" title="slideshow_images" href="/uploads/images/form/keeper2.jpg" rel="gallary_s">
+									<img style="width: 190px;" class="image" src="/uploads/images/form/keeper2.jpg" alt="Banants Stadium" />
+								</a>
+							</div>
 						</div>
 						<div class="tshirt">
-							<img src="../../../uploads/images/form/12.jpg" alt="" />
+							<div class="">
+								<a class="fancybox" title="slideshow_images" href="/uploads/images/form/wihte.jpg" rel="gallary_s">
+									<img style="width: 190px;" class="image" src="/uploads/images/form/wihte.jpg" alt="Banants Stadium" />
+								</a>
+							</div>
+							<div class="">
+								<a class="fancybox" title="slideshow_images" href="/uploads/images/form/keeper1.png" rel="gallary_s">
+									<img style="width: 190px;" class="image" src="/uploads/images/form/keeper1.png" alt="Banants Stadium" />
+								</a>
+							</div>
 						</div>
 					</div>
 				</div>

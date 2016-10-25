c763644df4ed8fce70c2fc97bc3d6fc3d9454a09
ArmSALArmy <arsenchikh@gmail.com>
2016-10-25 20:04:39 +0400

    -Menu Color change

--- views/test/Test_2.php
+++ views/test/Test_2.php
@@ -1,31 +1,86 @@
-<?
-
-use Ivliev\Imagefly\Imagefly;
-
-?>
-
-<div class="photo-galleries-item">
-    <div class="leftbar_images_slider_wrapper">
-        <a href=""><div class="h3">Лучшие видео игры Бананц-Улисс</div></a>
-        <div class="leftbar_images_slider clearfix">
-            <div class="leftbar_images_slider_item">
-                <div class="leftbar_slider_images leftbar_slider_image_1">
-                    <a class="fancybox" href="/uploads/images/photoGallery4/28671815913_5c5f049f28_z.jpg" title="FC Banants" rel="gallary_a">
-                        <img class="photo-gallery-image" src="<?=Imagefly::imagePath('/uploads/images/photoGallery4/28671815913_5c5f049f28_z.jpg', 'w280-q52')?>" alt="leftbar_images_slider_images">
-                    </a>
+<div class="inner_content_wrapper">
+    <div class="inner_content">
+        <div class="daily_news_page">
+            <div class="personal_info clearfix">
+                <div class="person">
+                    <div class="person_images_wrapper pictures_wrapper"><a class="fancybox" title="Шахназарян Эдвард" href="../../../uploads/images/staff/Edward_Shahnazaryan.jpg" rel="gallary_p"> <img class="image" src="../../../uploads/images/staff/Edward_Shahnazaryan.jpg" alt="leftbar_images_slider_images" /> </a></div>
+                    <div class="person_contacts">
+                        <div class="person_contacts_info">
+                            <h3>Шахназарян Эдвард Арамович</h3>
+                            <p>Кладовик-поставщик</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="leftbar_slider_images leftbar_slider_image_2">
-                    <a class="fancybox" href="/uploads/images/photoGallery4/29213968131_5ceb19149a_z.jpg" title="FC Banants" rel="gallary_a">
-                        <img class="photo-gallery-image" src="<?=Imagefly::imagePath('/uploads/images/photoGallery4/29213968131_5ceb19149a_z.jpg', 'w280-q52')?>" alt="leftbar_images_slider_images">
-                    </a>
+                <div class="person">
+                    <div class="person_images_wrapper pictures_wrapper"><a class="fancybox" title="Варданян Гоар" href="../../../uploads/images/staff/Gohar_Vardanyan.jpg" rel="gallary_p"> <img class="image" src="../../../uploads/images/staff/Gohar_Vardanyan.jpg" alt="leftbar_images_slider_images" /> </a></div>
+                    <div class="person_contacts">
+                        <div class="person_contacts_info">
+                            <h3>Варданян Гоар Вардановна</h3>
+                            <p>Помощница генерального директора</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="leftbar_slider_images leftbar_slider_image_3">
-                    <a class="fancybox" href="/uploads/images/photoGallery4/29213969741_f8f883157a_z.jpg" title="FC Banants" rel="gallary_a">
-                        <img class="photo-gallery-image" src="<?=Imagefly::imagePath('/uploads/images/photoGallery4/29213969741_f8f883157a_z.jpg', 'w280-q52')?>" alt="leftbar_images_slider_images">
-                    </a>
+            </div>
+            <div class="personal_info clearfix">
+                <div class="person">
+                    <div class="person_images_wrapper pictures_wrapper"><a class="fancybox" title="Овакимян Ишхан Ваунович" href="../../../uploads/images/staff/Ishkhan_Hovakimyan.jpg" rel="gallary_p"> <img class="image" src="../../../uploads/images/staff/Ishkhan_Hovakimyan.jpg" alt="leftbar_images_slider_images" /> </a></div>
+                    <div class="person_contacts">
+                        <div class="person_contacts_info">
+                            <h3>Овакимян Ишхан Ваунович</h3>
+                            <p>Специалист ИТ</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="person">
+                    <div class="person_images_wrapper pictures_wrapper"><a class="fancybox" title="Тутян Манук Ашотович" href="../../../uploads/images/staff/Manuk_Tutyan.jpg" rel="gallary_p"> <img class="image" src="../../../uploads/images/staff/Manuk_Tutyan.jpg" alt="leftbar_images_slider_images" /> </a></div>
+                    <div class="person_contacts">
+                        <div class="person_contacts_info">
+                            <h3>Тутян Манук Ашотович</h3>
+                            <p>Заведующих хозяйством</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="personal_info clearfix">
+                <div class="person">
+                    <div class="person_images_wrapper pictures_wrapper"><a class="fancybox" title="Авагян Марине Гамарниковна" href="../../../uploads/images/staff/Marine_Avagyan.jpg" rel="gallary_p"> <img class="image" src="../../../uploads/images/staff/Marine_Avagyan.jpg" alt="leftbar_images_slider_images" /> </a></div>
+                    <div class="person_contacts">
+                        <div class="person_contacts_info">
+                            <h3>Авагян Марине Гамарниковна</h3>
+                            <p>Инспектор по кадрам</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="person">
+                    <div class="person_images_wrapper pictures_wrapper"><a class="fancybox" title="Енокян Оник Арайикович" href="../../../uploads/images/staff/Onik_Enokyan.jpg" rel="gallary_p"> <img class="image" src="../../../uploads/images/staff/Onik_Enokyan.jpg" alt="leftbar_images_slider_images" /> </a></div>
+                    <div class="person_contacts">
+                        <div class="person_contacts_info">
+                            <h3>Енокян Оник Арайикович</h3>
+                            <p>Главный бухгальтер</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="personal_info clearfix">
+                <div class="person">
+                    <div class="person_images_wrapper pictures_wrapper"><a class="fancybox" title="Авагян Марине Гамарниковна" href="../../../uploads/images/staff/RomanShakaryan.jpg" rel="gallary_p"> <img class="image" src="../../../uploads/images/staff/RomanShakaryan.jpg" alt="leftbar_images_slider_images" /> </a></div>
+                    <div class="person_contacts">
+                        <div class="person_contacts_info">
+                            <h3>Роман Шакарян</h3>
+                            <p>Менеджер спорткомплекса</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="person">
+                    <div class="person_images_wrapper pictures_wrapper"><a class="fancybox" title="Ռոման Շաքարյյան" href="../../../uploads/images/staff/Simon_Israelyan.jpg" rel="gallary_p"> <img class="image" src="../../../uploads/images/staff/RomanShakaryan.jpg" alt="leftbar_images_slider_images" /> </a></div>
+                    <div class="person_contacts">
+                        <div class="person_contacts_info">
+                            <h3>Исрайелян Симон</h3>
+                            <p>Заведующих хозяйством</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <span class="photo-galery-link"><a href="https://www.flickr.com/photos/133834241@N06/albums">Показать больше >> </a></span>
     </div>
 </div>

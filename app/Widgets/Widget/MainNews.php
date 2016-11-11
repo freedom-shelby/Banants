fb527fb868c4d2f0c9ece26fd57084dc9b5908cf
ArmSALArmy <arsenchikh@gmail.com>
2016-11-11 09:36:58 +0400

    News Pages Filling Anon Widget fix

--- app/Widgets/Widget/MainNews.php
+++ app/Widgets/Widget/MainNews.php
@@ -73,45 +73,19 @@ class MainNews extends AbstractWidget{
     {
         $this->_param = json_decode($model->param, true);
 
-        if(App::instance()->getCurrentSlug() == 'home')
-        {
-            // Матерялов из клуба (от 1-го до 8-го матеряла)
-            $data = ArticleModel::find(Setting::instance()->getSettingVal('main_articles.category_id'))
-                ->descendants()
-                ->where('photo_id', '!=' , 1)
-                ->reOrderBy('created_at', 'desc')
-                ->limit($this->_param['settings']['max_news_limit'])
-                ->offset(4)->get(); // todo:: Anonsi Limitic sksi offset@
+        // Матерялов из клуба (от 1-го до 8-го матеряла)
+        $data = ArticleModel::find(Setting::instance()->getSettingVal('main_articles.category_id'))
+            ->descendants()
+            ->where('photo_id', '!=' , 1)
+            ->reOrderBy('created_at', 'desc')
+            ->limit($this->_param['settings']['max_news_limit'])
+            ->offset(4)->get(); // todo:: Anonsi Limitic sksi offset@
 
-            foreach ($data as $item) {
-                $this->_items[] = $item;
-            }
-
-            $this->_items = array_chunk($this->_items, $this->_param['settings']['news_per_page'], true);
-        }else{ // todo:: hanel ifelse -@
-               // todo:: arandzin Widget Sarqel
-            // Матерялов из slug-а (от 1-го до 8-го матеряла)
-            $data = ArticleModel::whereSlug(App::instance()->getCurrentSlug())->first()
-                ->ancestorsAndSelf()
-                ->whereLvl(Setting::instance()->getSettingVal('main_articles.category_level'))->first()
-                ->descendants()
-                ->where('photo_id', '!=' , 1)
-                ->reOrderBy('created_at', 'desc')
-                ->limit(16)->get();
-//        $data = ArticleModel::whereStatus(1)->has('defaultImages');
-
-            foreach ($data as $item) {
-                $this->_items[] = $item;
-            }
-
-            // todo: надо сделать Pagination
-            $this->_items = array_chunk($this->_items, 8, true);
+        foreach ($data as $item) {
+            $this->_items[] = $item;
         }
 
-//foreach ($data as $item) {
-//    echo "<pre>";
-//    print_r($item->toArray());
-//}die;
+        $this->_items = array_chunk($this->_items, $this->_param['settings']['news_per_page'], true);
 
         $this->_position = $model->position;
         $this->_sort = $model->sort;

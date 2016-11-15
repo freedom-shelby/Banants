dc55f4d87b8c6bf31a37999cf8854dc2a67e1860
ArmSALArmy <arsenchikh@gmail.com>
2016-11-15 12:54:02 +0400

    Integrait Google Webmaster and analitic

--- views/front/index.php
+++ views/front/index.php
@@ -94,80 +94,6 @@ use Helpers\Uri;
         </div><!-- inner -->
     </div><!-- container -->
 
-
-<div class="footer">
-<!--    <div class="inner clearfix">-->
-<!--        <div class="footer_col">-->
-<!--            <div class="footer_col_title">-->
-<!--                <h3>Клуб</h3>-->
-<!--            </div>-->
-<!--            <ul>-->
-<!--                <li><a href="#">История</a></li>-->
-<!--                <li><a href="#">Достижения</a></li>-->
-<!--                <li><a href="#">Президент</a></li>-->
-<!--                <li><a href="#">Сотрудники</a></li>-->
-<!--            </ul>-->
-<!--        </div>-->
-<!--        <div class="footer_col">-->
-<!--            <div class="footer_col_title">-->
-<!--                <h3>Новости</h3>-->
-<!--            </div>-->
-<!--            <ul>-->
-<!--                <li><a href="#">РА чемпионат</a></li>-->
-<!--                <li><a href="#">РА кубок</a></li>-->
-<!--                <li><a href="#">Евролига</a></li>-->
-<!--                <li><a href="#">Архив</a></li>-->
-<!--            </ul>-->
-<!--        </div>-->
-<!--        <div class="footer_col">-->
-<!--            <div class="footer_col_title">-->
-<!--                <h3>Премьер-лига</h3>-->
-<!--            </div>-->
-<!--            <ul>-->
-<!--                <li><a href="#">Игроки</a></li>-->
-<!--                <li><a href="#">Тренеры</a></li>-->
-<!--                <li><a href="#">График</a></li>-->
-<!--                <li><a href="#">Результаты</a></li>-->
-<!--            </ul>-->
-<!--        </div>-->
-<!--        <div class="footer_col">-->
-<!--            <div class="footer_col_title">-->
-<!--                <h3>Первая лига</h3>-->
-<!--            </div>-->
-<!--            <ul>-->
-<!--                <li><a href="#">Игроки</a></li>-->
-<!--                <li><a href="#">Тренеры</a></li>-->
-<!--                <li><a href="#">График</a></li>-->
-<!--                <li><a href="#">Результаты</a></li>-->
-<!--            </ul>-->
-<!--        </div>-->
-<!--        <div class="footer_col">-->
-<!--            <div class="footer_col_title">-->
-<!--                <h3>РА кубок</h3>-->
-<!--            </div>-->
-<!--            <ul>-->
-<!--                <li><a href="#">Кубок  2014/2015</a></li>-->
-<!--                <li><a href="#">График</a></li>-->
-<!--                <li><a href="#">Результаты</a></li>-->
-<!--                <li><a href="#">Бананц''–кубок</a></li>-->
-<!--                <li><a href="#">РА обладатели кубков</a></li>-->
-<!---->
-<!--            </ul>-->
-<!--        </div>-->
-<!--        <div class="footer_col">-->
-<!--            <div class="footer_col_title">-->
-<!--                <h3>юношеский футбол</h3>-->
-<!--            </div>-->
-<!--            <ul>-->
-<!--                <li><a href="#">Чемпионат</a></li>-->
-<!--                <li><a href="#">Достижения</a></li>-->
-<!--                <li><a href="#">Поступление</a></li>-->
-<!--                <li><a href="#">Тренеры</a></li>-->
-<!--                <li><a href="#">Программы</a></li>-->
-<!--            </ul>-->
-<!--        </div>-->
-<!--    </div>
-</div><!-- footer -->
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -176,7 +102,6 @@ use Helpers\Uri;
 
         ga('create', 'UA-86502245-1', 'auto');
         ga('send', 'pageview');
-
     </script>
 </body>
 </html>

c37fa3293af57d0d9b2a7b8005105a8826842bb6
ArmSALArmy <arsenchikh@gmail.com>
2017-02-08 17:16:47 +0400

    Update PDF's

--- views/front/widgets/bestPlayer.php
+++ views/front/widgets/bestPlayer.php
@@ -11,13 +11,13 @@
         <h2><?=__('BEST PLAYER')?></h2>
     </div>
     <div class="best_player_content">
-        <h3><?=__('Zhirayr')?> <?=__('Margaryan')?></h3>
+        <h3><?=__('Walter')?> <?=__('Poghosyan')?></h3>
         <div class="best_player_images_wrapper">
             <img src="/uploads/tmp/Valter_ARMBackFLAG.png" alt="best_player" />
         </div>
         <div class="best_player_right">
             <div class="best_player_info">
-                <span><?=__('Instat Index')?> 157<br> <?=__('Goals')?> 1<br> <?=__('Shots')?> 2</span>
+                <span><?=__('Instat Index')?> 157<br> <?=__('Goals')?> 0<br> <?=__('Shots')?> 0</span>
 <!--                <span class="best-player-goalkeeper">--><?//=__('Saves')?><!-- 2<br> --><?//=__('Passes')?><!-- 62 (87 %)<br> --><?//=__('Fight in the air')?><!-- 2 (100 %)</span>-->
             </div>
             <div class="best_player_multiple clearfix">

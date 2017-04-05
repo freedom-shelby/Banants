70680a08ffbf8eee960143bd413d65c432c440f3
ArmSALArmy <arsenchikh@gmail.com>
2017-04-05 15:28:49 +0400

    FIX News list with Article Status

--- views/front/widgets/academyAnons.php
+++ views/front/widgets/academyAnons.php
@@ -13,7 +13,7 @@ use Ivliev\Imagefly\Imagefly;
 
 <div class="academy-anons">
     <a href="<?=Uri::makeUriFromId('academy_squads')?>" title="slideshow_image" rel="gallary">
-        <img src="<?=Imagefly::imagePath('/uploads/images/academy/29184326872_e104f290e6_k.jpg', 'w445-h346-c-q60')?>" alt="container_top_slideshow_images" />
+        <img src="<?=Imagefly::imagePath('/uploads/images/academy/Image-2017-04-05.jpg', 'w445-h346-c-q60')?>" alt="container_top_slideshow_images" />
         <div class="container_top_slider_text">
             <div class="container_top_slider_text_inner">
                 <?= __('Academy') ?>
